d29bff78ee2504fc2a53d8ef0578cab7b0d71047
wgoaiaogw <348529249@qq.com>
2018-04-14 05:32:35 -0500

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,32 @@
+﻿var IMONumber;
+var IMOURL = "https://nextgen.lloydslistintelligence.com/vessels/?term=";
+function IsIMOOk(isIMO){
+	if (/^[0-9]*[1-9][0-9]*$/.test(isIMO) && (isIMO.length == 7)) {	//正则判断是否为正整数且长度为7
+		return true;
+	}
+	else{
+		//alert("IMO号码不正确");
+		return false;
+	}
+}
+
+
+
+document.getElementById('test1').onclick = function(){
+	IMONumber = document.getElementById('IMO').value;
+	if (IsIMOOk(IMONumber)){
+		chrome.tabs.create({
+			url: IMOURL + IMONumber
+		});
+	}
+
+}
+document.getElementById('test2').onclick = function(){
+	IMONumber = document.getElementById('IMO').value;
+	if (IsIMOOk(IMONumber)){
+		chrome.tabs.create({
+			url: IMOURL + IMONumber,
+			active:false
+		});
+	}
+}
