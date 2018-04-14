8be51f06d724d464d0ce38f6956da4048b22bf6a
wgoaiaogw <348529249@qq.com>
2018-04-14 05:33:57 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,10 @@
+﻿function login(){
+	var username = document.getElementById('thePage:siteTemplate:j_id27:username');
+	username.value = "XXXXXX";
+	var password = document.getElementById('thePage:siteTemplate:j_id27:password');
+	password.value = "XXXXXX";
+	var loginsubmit = document.getElementsByClassName("btn btn-block btn-primary");
+	loginsubmit[0].click();
+}
+
+login();
